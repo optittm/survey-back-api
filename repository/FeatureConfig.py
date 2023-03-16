031e054fed0d2453e7262b9dba523c8a6d165787
frederic.dymko <frederic.dymko@sii.net>
2023-03-16 12:22:06 +0100

    Added the FeatureConfig class that includes the getRulesConfig, getRuleFromFeature, and getProjectNameFromFeature methods, along with the Rule class and corresponding unit tests.

---
+++ repository/FeatureConfig.py
@@ -0,0 +1,98 @@
+
+import yaml
+from schema import Schema, SchemaError, Optional
+from yaml.loader import SafeLoader
+from models.Rule import Rule
+
+
+class FeatureConfig:
+
+    _FEATURE_CONFIG = 'rules.yaml'
+
+    _RULES_CONFIG_SCHEMA = {
+        Optional("get_comments_allowed_origins"): str,
+        Optional("front_allowed_origins"): str,
+        "projects": {
+            str: {
+                "rules": [{
+                    "feature_url": str,
+                    "ratio": float,
+                    "delay_before_reanswer": int,
+                    "delay_to_answer": int,
+                    "is_active": bool
+                }]
+            }
+        }
+    }
+
+    @staticmethod
+    def getRulesConfig(file_name: str = _FEATURE_CONFIG):
+        """
+        Loads the rule configuration data from the specified YAML file and validates it against the specified schema using the Schema library.
+
+        Args:
+            file_name (str): The name of the YAML file to load.
+
+        Returns:
+            dict: A dictionary containing the rule configuration data if the file exists and is valid against the schema.
+            None if the file does not exist or if it is not valid against the schema.
+        """
+        try:
+            with open(file_name) as f:
+                data = yaml.load(f, Loader=SafeLoader)
+                schema = Schema(FeatureConfig._RULES_CONFIG_SCHEMA, ignore_extra_keys=True)
+                schema.validate(data)
+                return data
+        except FileNotFoundError:
+            print(f"File {file_name} not found")
+            return None
+        except SchemaError as exc:
+            print(f"Error while validating the file {file_name}: {exc}")
+            return None
+        except yaml.YAMLError as exc:
+            print(f"Error while parsing the file {file_name}: {exc}")
+            return None
+        
+    @staticmethod
+    def getRuleFromFeature(feature_url: str):
+        """
+        Returns a Rule object corresponding to the specified feature URL, or None if the feature does not exist in the rule configuration.
+
+        Args:
+        feature_url (str): The URL of the feature for which to retrieve the rule.
+
+        Returns:
+        Rule: A Rule object containing the information for the corresponding rule, if it exists.
+            None if the feature does not exist in the rule configuration.
+        """
+        data = FeatureConfig.getRulesConfig(FeatureConfig._FEATURE_CONFIG)
+        if data :
+            for project_name, project_data in data['projects'].items():
+                for rule in project_data['rules']:
+                    if rule['feature_url'] == feature_url:
+                        return Rule(rule['feature_url'], \
+                                    rule['ratio'], \
+                                    rule['delay_before_reanswer'], \
+                                    rule['delay_to_answer'], \
+                                    rule['is_active'])
+        return None
+
+    @staticmethod
+    def getProjectNameFromFeature(feature_url: str):
+        """
+        Returns the name of the project to which the specified feature URL belongs, or None if the feature does not exist in the rule configuration.
+
+        Args:
+        feature_url (str): The URL of the feature for which to retrieve the project name.
+
+        Returns:
+        str: The name of the project to which the feature belongs, if it exists.
+            None if the feature does not exist in the rule configuration.
+        """
+        data = FeatureConfig.getRulesConfig(FeatureConfig._FEATURE_CONFIG)
+        if data :
+            for project_name, project_data in data['projects'].items():
+                for rule in project_data['rules']:
+                    if rule['feature_url'] == feature_url:
+                        return project_name
+        return None
--- utils/container.py
+++ utils/container.py
@@ -2,6 +2,8 @@ from dependency_injector import containers, providers
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 
+from repository.FeatureConfig import FeatureConfig
+
 class Container(containers.DeclarativeContainer):
     load_dotenv()
 
@@ -9,3 +11,5 @@ class Container(containers.DeclarativeContainer):
 
     Session = sessionmaker()
     db_session = providers.Singleton(Session)
+
+    features_config = providers.Factory(FeatureConfig)
