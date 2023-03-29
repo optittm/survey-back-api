fc51aa09c5360e92f30a1fc26d3876e2fa4b76b4
Yann Le Coz <ylcoz@icloud.com>
2023-03-29 16:29:53 +0200

    Set Cookies user_id and timestamp

--- routes/rules.py
+++ routes/rules.py
@@ -1,25 +1,37 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 from dependency_injector.wiring import inject
 from models.Rule import Rule
 from repository.FeatureConfig import FeatureConfig
 
-from datetime import date, timedelta 
+from datetime import date, datetime, timedelta
+from uuid import uuid4
 import random
 
 router = APIRouter()
 
 @router.get("/rules")
 @inject
-def show_modal(featureUrl: str, request: Request) -> bool:
+def show_modal(featureUrl: str, request: Request, response: Response) -> bool:
     # Get rules from featureUrl query
     rulesFromFeature: Rule | None = FeatureConfig.getRuleFromFeature(feature_url=featureUrl)
 
+    # Get Cookie user_id
+    userCookie: str | None = request._cookies.get('user_id')
+    # Set user_id Cookie if is None
+    if(userCookie == None):
+        response.set_cookie(key="user_id", value=str(uuid4()))
+
     # Get current timestamp
     dateToday: date = date.today()
-    # Get Cookie timestamp
+    # Get Cookie timestamp and format into date
     dateTimestamp: date = date.fromtimestamp(float(request._cookies.get('timestamp')))
 
     isOverDelay: bool = timedelta(days=rulesFromFeature.delay_before_reanswer * 30.5) <= dateToday - dateTimestamp
-    isWithinRatio = random.random() <= rulesFromFeature.ratio
+    isWithinRatio: bool = random.random() <= rulesFromFeature.ratio
+    isDisplay: bool = rulesFromFeature.is_active and isOverDelay and isWithinRatio
 
-    return rulesFromFeature.is_active and isOverDelay and isWithinRatio
+    # Set timestamp Cookie to current timestamp when display survey modal
+    if(isDisplay):
+        response.set_cookie(key="timestamp", value=datetime.now().timestamp())
+
+    return isDisplay
