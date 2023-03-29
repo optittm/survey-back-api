4173c21d46145fabc92d0ef58d4b14869bbf40be
Yann Le Coz <ylcoz@icloud.com>
2023-03-29 12:13:05 +0200

    Set conditions of display survey modal

--- routes/rules.py
+++ routes/rules.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from dependency_injector.wiring import inject
 from models.Rule import Rule
 from repository.FeatureConfig import FeatureConfig
+
+from datetime import date, timedelta 
+import random
 
 router = APIRouter()
 
 @router.get("/rules")
 @inject
-def show_modal(featureUrl: str) -> bool:
+def show_modal(featureUrl: str, request: Request) -> bool:
     # Get rules from featureUrl query
     rulesFromFeature: Rule | None = FeatureConfig.getRuleFromFeature(feature_url=featureUrl)
 
-    return rulesFromFeature.is_active
+    # Get current timestamp
+    dateToday: date = date.today()
+    # Get Cookie timestamp
+    dateTimestamp: date = date.fromtimestamp(float(request._cookies.get('timestamp')))
+
+    isOverDelay: bool = timedelta(days=rulesFromFeature.delay_before_reanswer * 30.5) <= dateToday - dateTimestamp
+    isWithinRatio = random.random() <= rulesFromFeature.ratio
+
+    return rulesFromFeature.is_active and isOverDelay and isWithinRatio
