20e0979ef9d475e0c54fe8a52c1993b513b4a091
Antoine Micard <33573640+Creadeyh@users.noreply.github.com>
2023-03-30 11:41:18 +0200

    Fixed compatibility with Python 3.8

--- routes/comments.py
+++ routes/comments.py
@@ -1,15 +1,21 @@
-from typing import List
+from typing import List, Union
 from fastapi import APIRouter, Depends, status, Cookie
+from dependency_injector.wiring import Provide, inject
+
 from models.comment import Comment, CommentPostBody
 from utils.container import Container
-from dependency_injector.wiring import Provide, inject
 from repository.sqlite_repository import SQLiteRepository
 
 router = APIRouter()
 
 @router.post("/comments", status_code=status.HTTP_201_CREATED,response_model=Comment)
 @inject
-async def create_comment(commentcookie: CommentPostBody, user_id: str | None= Cookie(default=None), timestamp: str | None = Cookie(default=None), comment_repo: SQLiteRepository = Depends(Provide[Container.db_manager])) -> Comment:
+async def create_comment(
+        commentcookie: CommentPostBody,
+        user_id: Union[str, None] = Cookie(default=None),
+        timestamp: Union[str, None] = Cookie(default=None),
+        comment_repo: SQLiteRepository = Depends(Provide[Container.db_manager])
+    ) -> Comment:
     # TODO: get domain name from request instead
     project_name = commentcookie.feature_url.split('/')[2]
     new_comment = await comment_repo.create_comment(commentcookie, user_id, timestamp, project_name)
