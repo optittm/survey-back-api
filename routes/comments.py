0d1bd04f9f9134fc984b58d38cf615a81b49f819
Antoine Micard <33573640+Creadeyh@users.noreply.github.com>
2023-04-03 17:25:33 +0200

    Use project name from YAML

--- routes/comments.py
+++ routes/comments.py
@@ -1,25 +1,31 @@
 from typing import List, Union
-from fastapi import APIRouter, Depends, status, Cookie
+from fastapi import APIRouter, Depends, status, Cookie, Response
 from dependency_injector.wiring import Provide, inject
 
 from models.comment import Comment, CommentPostBody
 from utils.container import Container
 from repository.sqlite_repository import SQLiteRepository
+from repository.yaml_rule_repository import YamlRulesRepository
 
 router = APIRouter()
 
-@router.post("/comments", status_code=status.HTTP_201_CREATED,response_model=Comment)
+@router.post("/comments", status_code=status.HTTP_201_CREATED, response_model=Union[Comment, dict])
 @inject
 async def create_comment(
         comment_body: CommentPostBody,
+        response: Response,
         user_id: Union[str, None] = Cookie(default=None),
         timestamp: Union[str, None] = Cookie(default=None),
-        sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo])
+        sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo]),
+        rules_config: YamlRulesRepository = Depends(Provide[Container.rules_config])
     ) -> Comment:
-    # TODO: get domain name from request instead
-    project_name = comment_body.feature_url.split('/')[2]
-    new_comment = await sqlite_repo.create_comment(comment_body, user_id, timestamp, project_name)
-    return new_comment
+
+    if (project_name := rules_config.getProjectNameFromFeature(comment_body.feature_url)) is not None:
+        new_comment = await sqlite_repo.create_comment(comment_body, user_id, timestamp, project_name)
+        return new_comment
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"Error": "Feature not found"}
 
 @router.get("/comments",  response_model=List[Comment])
 @inject
