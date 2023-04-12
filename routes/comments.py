8cc2079db15809000ca152ab770b14b94db3c7cc
MelisandeDes <126159430+MelisandeDes@users.noreply.github.com>
2023-04-12 11:10:07 +0200

    Feat/sqlite (#5)
    * Change project_id to project_name and tests
    
    * Remove unused imports
    
    ---------
    
    Co-authored-by: FredericDymko <126159477+FredericDymko@users.noreply.github.com>

--- routes/comments.py
+++ routes/comments.py
@@ -2,10 +2,11 @@ from typing import List, Union
 from fastapi import APIRouter, Depends, status, Cookie, Response
 from dependency_injector.wiring import Provide, inject
 
-from models.comment import Comment, CommentPostBody
+from models.comment import Comment, CommentGetBody, CommentPostBody
 from utils.container import Container
 from repository.sqlite_repository import SQLiteRepository
 from repository.yaml_rule_repository import YamlRulesRepository
+from utils.formatter import comment_to_comment_get_body
 
 router = APIRouter()
 
@@ -27,8 +28,9 @@ async def create_comment(
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"Error": "Feature not found"}
 
-@router.get("/comments",  response_model=List[Comment])
+@router.get("/comments",  response_model=List[CommentGetBody])
 @inject
 async def get_all_comments(sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo])) -> List[Comment]:
     comments = await sqlite_repo.read_comments()
+    comments = [await comment_to_comment_get_body(comment) for comment in comments]
     return comments
