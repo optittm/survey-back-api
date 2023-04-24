96b5f5142c1f28e19c70a9bf60626d52d972d567
Antoine Micard <33573640+Creadeyh@users.noreply.github.com>
2023-04-24 10:21:25 +0200

    Logging (#10)
    * Initial logs
    
    * Logs part 2
    
    * Fix log config

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dependency_injector.wiring import Provide, inject
 import uvicorn
 from sqlalchemy.exc import ArgumentError
 from pydbantic import Database
+import logging
 
 from models.comment import Comment
 from models.project import Project, ProjectEncryption
@@ -16,6 +17,7 @@ from utils.container import Container
 
 @inject
 def init_fastapi(config=Provide[Container.config]) -> FastAPI:
+    logging.info("Init FastAPI app")
     # Creates the FastAPI instance inside the function to be able to use the config provider
     app = FastAPI()
     app.add_middleware(
@@ -43,6 +45,7 @@ async def main(config=Provide[Container.config]):
     # Running the server in the existing async loop
     # https://www.uvicorn.org/#config-and-server-instances
     server = uvicorn.Server(config)
+    logging.info("Starting uvicorn server")
     await server.serve()
 
 
@@ -56,12 +59,19 @@ async def init_db(
         db = await Database.create(
             config["survey_db"], tables=[Project, Comment, ProjectEncryption]
         )
+        logging.info("Database ready")
     except ArgumentError as e:
+        logging.error("Error initialising the database")
         raise Exception(f"Error from sqlalchemy : {str(e)}")
 
     project_names = rules_config.getProjectNames()
     for project_name in project_names:
         await sqlite_repo.create_project(Project(name=project_name))
+
+
+@inject
+def config_logging(config=Provide[Container.config]):
+    logging.basicConfig(level=config["log_level"])
 
 
 load_dotenv()
@@ -91,8 +101,10 @@ container.config.cors_allow_headers.from_env(
 container.config.debug_mode.from_env(
     "DEBUG_MODE", required=True, as_=bool, default=False
 )
+container.config.log_level.from_env("LOG_LEVEL", required=True, default="INFO")
 container.wire(modules=[__name__])
 
+config_logging()
 app = init_fastapi()
 app.container = container
 
--- utils/formatter.py
+++ utils/formatter.py
@@ -2,6 +2,7 @@ from models.comment import Comment, CommentGetBody
 from models.project import Project
 from dependency_injector.wiring import Provide
 from repository.sqlite_repository import SQLiteRepository
+import logging
 
 from utils.container import Container
 
@@ -13,7 +14,7 @@ async def comment_to_comment_get_body(
     Convert a Comment to a CommentGetBody with project name
     """
     project: Project = await sqliterepo.get_project_by_id(comment.project_id)
-    return CommentGetBody(
+    new_comment = CommentGetBody(
         id=comment.id,
         project_name=project.name,
         user_id=comment.user_id,
@@ -22,3 +23,5 @@ async def comment_to_comment_get_body(
         rating=comment.rating,
         comment=comment.comment,
     )
+    logging.debug(f"Formatted comment object to {new_comment}")
+    return new_comment
