14ed4e1f92450b45ff9f1cb00872460b07caf731
frederic.dymko <frederic.dymko@gmail.com>
2023-05-09 09:12:07 +0200

    Fix: Added the check if the project name exist

--- routes/project.py
+++ routes/project.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 
 from dependency_injector.wiring import Provide, inject
 from repository.sqlite_repository import SQLiteRepository
+from repository.yaml_rule_repository import YamlRulesRepository
 from utils.container import Container
 
 router = APIRouter()
@@ -12,8 +13,12 @@ router = APIRouter()
 @inject
 async def get_project_rating(
     id: int,
-    sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo])
+    sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo]),
+    yaml_repo: YamlRulesRepository = Depends(Provide[Container.rules_config])
 ) -> dict:  
+    project = await sqlite_repo.get_project_by_id(id)
+    if project.name not in yaml_repo.getProjectNames():
+        return {"id": id, "Error": "Project not found"}
     rating = sqlite_repo.get_project_avg_rating(id)
     return {
         "id": id,
