ea74c5bb9e06bb4b531f94912c2e79575572564f
frederic.dymko <frederic.dymko@gmail.com>
2023-05-09 10:26:28 +0200

    Fix: Added docstring

--- routes/project.py
+++ routes/project.py
@@ -16,6 +16,20 @@ async def get_project_rating(
     sqlite_repo: SQLiteRepository = Depends(Provide[Container.sqlite_repo]),
     yaml_repo: YamlRulesRepository = Depends(Provide[Container.rules_config])
 ) -> dict:  
+    """
+    Retrieve the average rating of a project with the given id.
+    
+    Args:
+        id (int): The id of the project to retrieve the rating for.
+        sqlite_repo (SQLiteRepository, optional): The SQLiteRepository instance to use for database access.
+            Defaults to Depends(Provide[Container.sqlite_repo]).
+        yaml_repo (YamlRulesRepository, optional): The YamlRulesRepository instance to use for rules configuration.
+            Defaults to Depends(Provide[Container.rules_config]).
+    
+    Returns:
+        dict: A dictionary containing the project id and its average rating, or an error message if the project
+        was not found or if its name is not included in the list of project names in the YamlRulesRepository.
+    """
     project = await sqlite_repo.get_project_by_id(id)
     if not project or project.name not in yaml_repo.getProjectNames():
         return {"id": id, "Error": "Project not found"}
