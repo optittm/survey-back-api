7d11d06e18a6bc437be5dd373facf6bbb8bcc667
frederic.dymko <126159477+FredericDymko@users.noreply.github.com>
2023-05-10 14:40:33 +0200

    feat: Added the warehouse data (#16)
    * Feat: Added the storage to store when the modal is displayed
    
    * feat: Added the warehouse data
    
    * Fix: Correction of the docstring
    
    * Fix: Moving the db_warehouse file from utils to repository
    
    * Fix: DBWarehouse methode now only need project_id
    
    * Fix: Adding a container for DBWarehouse
    
    * Fix: Moving the DBWarehouse in the SQLiteRepository class.
    
    * Fix: Make function return 0 if there is no match.
    
    ---------
    
    Co-authored-by: frederic.dymko <frederic.dymko@gmail.com>
    Co-authored-by: Antoine Micard <33573640+Creadeyh@users.noreply.github.com>

--- repository/sqlite_repository.py
+++ repository/sqlite_repository.py
@@ -1,6 +1,7 @@
 
 from typing import List, Union
 import logging
+import sqlite3
 
 from models.comment import Comment, CommentPostBody
 from models.display import Display
@@ -9,6 +10,189 @@ from utils.encryption import Encryption
 
 
 class SQLiteRepository:
+
+    def __init__(self, config):
+        self.db_name = config["survey_db"].replace("sqlite:///", "")
+
+        self.__create_view()
+
+    def __create_view(self):
+        """
+        Creates views in the database to calculate statistics on project comments and displays.
+
+        This method creates four views in the database:
+            - 'feature_rating_avg': calculates the average rating for each feature of a project from comments.
+            - 'project_rating_avg': calculates the average rating for each project from comments.
+            - 'number_comment_by_project': calculates the number of comments for each project.
+            - 'number_display_by_project': calculates the number of displays for each project.
+
+        If a view already exists in the database, the method ignores its creation.
+        """
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        # All the creation of the view are ignored if the view already exist
+        # Create the view that count the average rating of a feature of a project
+        cursor.execute('''
+            CREATE VIEW IF NOT EXISTS feature_rating_avg AS
+                SELECT project_id, feature_url, AVG(rating) AS average_rating
+                FROM Comment
+                GROUP BY project_id, feature_url;
+        ''')
+        # Create the view that count the average rating of a project
+        cursor.execute('''
+            CREATE VIEW IF NOT EXISTS project_rating_avg AS
+                SELECT project_id, AVG(rating) AS average_rating
+                FROM Comment
+                GROUP BY project_id;
+        ''')
+        # Create the view that count the number of comment by project
+        cursor.execute('''
+            CREATE VIEW IF NOT EXISTS number_comment_by_project AS
+                SELECT project_id, count(timestamp) AS number_comment
+                FROM Comment
+                GROUP BY project_id;
+        ''')
+        # Create the view that count the number of display by project
+        cursor.execute('''
+            CREATE VIEW IF NOT EXISTS number_display_by_project AS
+                SELECT project_id, count(timestamp) AS number_display
+                FROM Display
+                GROUP BY project_id;
+        ''')
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def get_project_avg_rating(self, project_id: int):
+        """
+        Retrieve the average rating of a project from the `project_rating_avg` view.
+
+        Args:
+            project (Project): The project object for which to retrieve the average rating.
+
+        Returns:
+            float: The average rating of the project.
+
+        Raises:
+            IndexError: If the result of the query is empty.
+        """
+        
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            SELECT average_rating 
+            FROM project_rating_avg
+            WHERE project_id = {project_id};
+        ''')
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        try:
+            return result[0][0]
+        except IndexError:
+            return 0
+
+    def get_feature_avg_rating(self, project_id: int, feature_url: str):
+        """
+        Retrieves the average rating for a given feature of a specific project from 
+        the database.
+
+        This method executes an SQL query to retrieve the average rating associated 
+        with the specified feature of a specific project. The average rating is extracted 
+        from the 'feature_rating_avg' view created by the '__create_views' method.
+
+        Args:
+            project (Project): the Project object representing the project for which the 
+                average rating should be retrieved.
+            feature_url (str): the URL of the feature for which the average rating should 
+                be retrieved.
+
+        Returns:
+            float: the average rating of the specified feature for the specified project. 
+                If the average rating is not found, returns None.
+        """
+        
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            SELECT average_rating
+            FROM feature_rating_avg
+            WHERE project_id = {project_id}
+                AND feature_url = "{feature_url}";
+        ''')
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        try:
+            return result[0][0]
+        except IndexError:
+            return 0
+
+    def get_number_of_comment(self, project_id: int):
+        """
+        Retrieves the number of comments for a specific project from the database.
+
+        This method executes an SQL query to retrieve the number of comments associated 
+        with a specific project. The number of comments is extracted from the 
+        'number_comment_by_project' view created by the '__create_views' method.
+
+        Args:
+            project (Project): the Project object representing the project for which the 
+                number of comments should be retrieved.
+
+        Returns:
+            int: the number of comments for the specified project. If the number of comments 
+                is not found, returns None.
+        """
+        
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            SELECT number_comment
+            FROM number_comment_by_project
+            WHERE project_id = {project_id};
+        ''')
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        try:
+            return result[0][0]
+        except IndexError:
+            return 0
+
+    def get_number_of_display(self, project_id: int):
+        """
+        Retrieves the number of displays for a specific project from the database.
+
+        This method executes an SQL query to retrieve the number of displays associated 
+        with a specific project. The number of displays is extracted from the 
+        'number_display_by_project' view created by the '__create_views' method.
+
+        Args:
+            self: the instance of the class.
+            project (Project): the Project object representing the project for which the 
+                number of displays should be retrieved.
+
+        Returns:
+            int: the number of displays for the specified project. If the number of displays 
+                is not found, returns None.
+        """
+
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            SELECT number_display
+            FROM number_display_by_project
+            WHERE project_id = {project_id};
+        ''')
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        try:
+            return result[0][0]
+        except IndexError:
+            return 0
+
     async def create_comment(
         self,
         comment_body: CommentPostBody,
--- tests/test_sqlite_repository.py
+++ tests/test_sqlite_repository.py
@@ -1,6 +1,7 @@
+import sqlite3
 import unittest
 from datetime import datetime
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
 
 from models.comment import Comment, CommentPostBody
 from models.display import Display
@@ -9,8 +10,74 @@ from repository.sqlite_repository import SQLiteRepository
 
 
 class TestSQLiteRepository(unittest.IsolatedAsyncioTestCase):
+
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.db_name = 'tests/mocks/test_db'
+        cls.conn = sqlite3.connect(cls.db_name)
+        cls.cursor = cls.conn.cursor()
+
+        # create test tables
+        cls.cursor.execute('''
+            CREATE TABLE Project (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL
+            );
+        ''')
+
+        cls.cursor.execute('''
+            CREATE TABLE Comment (
+                id INTEGER PRIMARY KEY,
+                project_id INTEGER NOT NULL,
+                feature_url TEXT NOT NULL,
+                rating INTEGER NOT NULL,
+                timestamp DATETIME NOT NULL,
+                FOREIGN KEY(project_id) REFERENCES Project(id)
+            );
+        ''')
+
+        cls.cursor.execute('''
+            CREATE TABLE Display (
+                id INTEGER PRIMARY KEY,
+                project_id INTEGER NOT NULL,
+                timestamp DATETIME NOT NULL,
+                FOREIGN KEY(project_id) REFERENCES Project(id)
+            );
+        ''')
+
+        cls.cursor.execute('''
+            INSERT INTO Project (id, name) VALUES
+            (1, 'Project A'),
+            (2, 'Project B');
+        ''')
+
+        cls.cursor.execute('''
+            INSERT INTO Comment (id, project_id, feature_url, rating, timestamp) VALUES
+            (1, 1, 'http://example.com/feature1', 3, '2023-05-01 10:00:00'),
+            (2, 1, 'http://example.com/feature1', 5, '2023-05-01 11:00:00'),
+            (3, 1, 'http://example.com/feature2', 4, '2023-05-01 12:00:00'),
+            (4, 2, 'http://example.com/feature1', 2, '2023-05-01 13:00:00');
+        ''')
+
+        cls.cursor.execute('''
+            INSERT INTO Display (id, project_id, timestamp) VALUES
+            (1, 1, '2023-05-01 10:00:00'),
+            (2, 1, '2023-05-01 11:00:00'),
+            (3, 2, '2023-05-01 12:00:00');
+        ''')
+
+        cls.conn.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.cursor.execute('DROP TABLE Comment')
+        cls.cursor.execute('DROP TABLE Display')
+        cls.cursor.execute('DROP TABLE Project')
+
     async def asyncSetUp(self):
-        self.repository = SQLiteRepository()
+        self.config = {"survey_db": self.db_name}
+        self.repository = SQLiteRepository(self.config)
         self.project_name = "test_project"
         self.comment_body = CommentPostBody(
             feature_url="http://test.com",
@@ -121,3 +188,31 @@ class TestSQLiteRepository(unittest.IsolatedAsyncioTestCase):
                 timestamp=timestamp_dt
             ),
         )
+
+        
+    def test_get_project_avg_rating(self):
+        project_a = Project(id=1, name='Project A')
+        project_b = Project(id=2, name='Project B')
+
+        self.assertAlmostEqual(self.repository.get_project_avg_rating(project_a.id), 4.0)
+        self.assertAlmostEqual(self.repository.get_project_avg_rating(project_b.id), 2.0)
+
+    def test_get_feature_avg_rating(self):
+        project_a = Project(id=1, name='Project A')
+
+        self.assertAlmostEqual(self.repository.get_feature_avg_rating(project_a.id, 'http://example.com/feature1'), 4.0)
+        self.assertAlmostEqual(self.repository.get_feature_avg_rating(project_a.id, 'http://example.com/feature2'), 4.0)
+
+    def test_get_number_of_comment(self):
+        project_a = Project(id=1, name='Project A')
+        project_b = Project(id=2, name='Project B')
+
+        self.assertEqual(self.repository.get_number_of_comment(project_a.id), 3)
+        self.assertEqual(self.repository.get_number_of_comment(project_b.id), 1)
+
+    def test_get_number_of_display(self):
+        project_a = Project(id=1, name='Project A')
+        project_b = Project(id=2, name='Project B')
+
+        self.assertEqual(self.repository.get_number_of_display(project_a.id), 2)
+        self.assertEqual(self.repository.get_number_of_display(project_b.id), 1)
