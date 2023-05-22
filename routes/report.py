0121f7d29906827968849919486497df7a625796
Yann Le Coz <ylcoz@icloud.com>
2023-05-22 11:15:28 +0200

    feat(survey-detail-project-report): Initialisation report
    - Add a endpoint to generate and show the HTML report
    - Passing project id to generate the report

--- routes/report.py
+++ routes/report.py
@@ -12,5 +12,11 @@ templates = jinja2.Template("templates")
 
 @router.get("/survey-report", response_class=HTMLResponse)
 async def init_report() -> str:
-    html_repository = HTMLRepository()
+    html_repository = HTMLRepository(reportFile="surveyReport.html")
     return html_repository.generate_report()
+
+
+@router.get("/survey-report/project/{id}", response_class=HTMLResponse)
+async def init_detail_project_report(id: str) -> str:
+    html_repository = HTMLRepository(reportFile="surveyProjectDetailReport.html")
+    return html_repository.generate_detail_project_report(id)
