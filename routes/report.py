e1ef44f0dc6b9133a44f924871e49731e0b6da30
Yann Le Coz <ylcoz@icloud.com>
2023-05-23 11:28:52 +0200

    Feat : Init static detail project report (#27)
    * feat(survey-detail-project-report): Initialisation report
    
    - Add a endpoint to generate and show the HTML report
    - Passing project id to generate the report
    
    * feat(survey-report-detail-project): Query Parameters
    
    - Put Query string to filter by date
    - Documentation this new route in README.md
    
    * Update Readme.md
    
    ---------
    
    Co-authored-by: MelisandeDes <melisande.desgranges@sii.fr>

--- routes/report.py
+++ routes/report.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from repository.html_repository import HTMLRepository
@@ -12,5 +13,15 @@ templates = jinja2.Template("templates")
 
 @router.get("/survey-report", response_class=HTMLResponse)
 async def init_report() -> str:
-    html_repository = HTMLRepository()
+    html_repository = HTMLRepository(reportFile="surveyReport.html")
     return html_repository.generate_report()
+
+
+@router.get("/survey-report/project/{id}", response_class=HTMLResponse)
+async def init_detail_project_report(
+    id: str, timestamp_start: Optional[str] = None, timestamp_end: Optional[str] = None
+) -> str:
+    html_repository = HTMLRepository(reportFile="surveyProjectDetailReport.html")
+    return html_repository.generate_detail_project_report(
+        id, timestamp_start, timestamp_end
+    )
