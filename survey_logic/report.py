03f77bac870e3e189c51bc98e6a60dce3a08be61
WylanLMSII <144223617+WylanLMSII@users.noreply.github.com>
2023-09-15 14:59:06 +0200

    Bar charts (#45)
    * added bar charts
    
    * Fixing normes
    
    * Adapting unit test for return
    
    * removing useless import
    
    * two graph (- curreny month - curent month and last two) indent

--- survey_logic/report.py
+++ survey_logic/report.py
@@ -8,6 +8,9 @@ from utils.html_report import HTMLReport
 from repository.sqlite_repository import SQLiteRepository
 from repository.yaml_rule_repository import YamlRulesRepository
 from utils.container import Container
+from datetime import datetime
+
+import numpy as np
 
 @inject
 async def generate_project_report(
@@ -57,6 +60,38 @@ async def generate_project_report(
 
     return html_repository.generate_report(projects)
 
+#Ponderates an array of notes by score and date
+#Curently the date is registered as DD/MM/YYYY so the granularity is daily
+#This function has been added to improve the graph design
+
+def ponderate(notes)->[] : 
+    if len(notes) <=0 :
+        return []
+    notes_pond=[]
+    notes=sorted(notes,key=lambda x: (x['note'], x['day']))
+    for note in notes : 
+        added = 0
+        i=0
+        for note_p in notes_pond :
+            
+            if note_p["day"] == note["day"] and note_p["note"] == note["note"]:
+                count=note_p["count"] +1
+                del notes_pond[i]
+                notes_pond.append({
+                    'note' : note["note"],
+                    'count' : count,
+                    'day' : note["day"],
+                })
+                added = 1
+            i +=1
+        if added == 0 : 
+            notes_pond.append({
+                'note' : note["note"],
+                'count' : note["count"],
+                'day' : note["day"],
+            })
+    return notes_pond
+
 @inject
 async def generate_detailed_report_from_project_id(
     project_id: int,
@@ -96,6 +131,7 @@ async def generate_detailed_report_from_project_id(
     graphs = []
     date_timestamp_start = []
     date_timestamp_end = []
+    notes_sec = []
 
     for feature_url, rates in filtered_rates.items():
         if feature_url == 'date_timestamp_start':
@@ -115,9 +151,70 @@ async def generate_detailed_report_from_project_id(
         graph_data = {
             "feature_url": feature_url,
             "comment_count": len(rates),
-            "figure_html": fig_html
+            "figure_html": fig_html,
         }
         graphs.append(graph_data)
+        notes_sec=[({
+            'note': str(rate["rate"]),
+            'count': 1,
+            'day':str(rate["date_timestamp"]),
+            }) for rate in rates]
+        notes_sec_pond=[]
+        #If empty the graph lib shows an error about x (first argument) that receive a bad format []
+        if(len(notes_sec)>0) :
+            notes_sec_pond=ponderate(notes_sec)
+            first_day_of_the_month = datetime.strptime(notes_sec_pond[0]['day'], '%Y-%m-%d').replace(day=1)
+            first_day_of_next_month = (datetime.strptime(notes_sec_pond[0]['day'], '%Y-%m-%d')+pd.DateOffset(months=1)).replace(day=1)
+            first_day_of_two_month_back=(datetime.strptime(notes_sec_pond[0]['day'], '%Y-%m-%d')-pd.DateOffset(months=2)).replace(day=1)
+            
+            n_p_month=[note for note in notes_sec_pond if datetime.strptime(note['day'], '%Y-%m-%d') >= first_day_of_the_month and datetime.strptime(note['day'], '%Y-%m-%d') < first_day_of_next_month]
+            n_p_three_month=[note for note in notes_sec_pond if datetime.strptime(note['day'], '%Y-%m-%d') >= first_day_of_two_month_back and datetime.strptime(note['day'], '%Y-%m-%d') < first_day_of_next_month]
+            fig__monthly = px.bar(
+                n_p_month, 
+                x="day", 
+                y="count", 
+                color="note", 
+                title="Totals notes", 
+                color_discrete_map={
+                    "1": "red", 
+                    "2": "orange", 
+                    "3": "yellow", 
+                    "4": "lightGreen", 
+                    "5": "green",
+            }   )
+            
+            fig_three_month = px.bar(
+                n_p_three_month, 
+                x="day", 
+                y="count", 
+                color="note", 
+                title="Totals notes", 
+                color_discrete_map={
+                    "1": "red", 
+                    "2": "orange", 
+                    "3": "yellow", 
+                    "4": "lightGreen", 
+                    "5": "green",
+            }   )
+            total = sum([note['count'] for note in notes_sec])
+            #For future use
+            moyenne = sum([note['count'] * int(note['note']) for note in notes_sec ]) / total
+            median = np.median(np.sort([int(note['note']) for note in notes_sec for _ in range(note['count'])]))
+
+            fig_html_monthly = fig__monthly.to_html(full_html=False, include_plotlyjs=False)
+            graph_data_monthly = {
+                "feature_url": feature_url,
+                "comment_count": total,
+                "figure_html": fig_html_monthly,
+            }
+            graphs.append(graph_data_monthly)
+            fig_html_three_month = fig_three_month.to_html(full_html=False, include_plotlyjs=False)
+            graph_data_three_month = {
+                "feature_url": feature_url,
+                "comment_count": total,
+                "figure_html": fig_html_three_month,
+            }
+            graphs.append(graph_data_three_month)
 
     return html_repository.generate_detail_project_report(
         project_id, timerange, date_timestamp_start, date_timestamp_end, graphs
